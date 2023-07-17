fc11b95e3f1f912f468b0d7f383e008c95d02b13
IzatheGreat15 <20100869@usc.edu.ph>
2023-07-17 13:44:26 +0800

    about view done

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,12 +1,14 @@
 <template>
   <main class="bg-img flex flex-col h-full" id="about">
     <Navbar />
-    <section class="flex-grow flex flex-col-reverse md:flex-row justify-end md:justify-center items-center bg-pink-200">
-      <div class="bg-slate-300 w-full md:w-2/3 px-2 md:px-20">
-        <h6 class="uppercase text-2xl md:text-4xl text-white font-extrabold bg-orange-200">Kathleen Iza</h6>
-        <h1 class="uppercase text-6xl md:text-9xl text-white font-extrabold bg-red-200">Monzales</h1>
-        <p class="md:text-xl font-medium text-white bg-orange-200">I'm a Software Developer</p>
-        <div class="bg-blue-300 md:w-1/2 my-5 md:my-10">
+    <section class="flex-grow flex flex-col-reverse gap-8 md:gap-0 md:flex-row justify-end md:justify-center items-center">
+      <div class="w-full md:w-2/3 px-6 md:px-20">
+        <h6 class="uppercase text-xl md:text-4xl text-white font-extrabold">Kathleen Iza</h6>
+        <h1 class="uppercase text-6xl md:text-9xl text-white font-extrabold">
+          Monzales
+        </h1>
+        <p class="md:text-xl font-medium text-white">I'm a Software Developer</p>
+        <div class="md:w-1/2 my-5 md:my-10">
           <!-- buttons -->
           <div class="flex">
             <button class="w-1/2 md:w-1/3 mr-2 bg-red-700 hover:bg-red-600 text-white font-semibold py-2 px-4 border border-transparent">
@@ -17,16 +19,24 @@
             </button>
           </div>
           <!-- icons -->
-          <div>
-            <span>1</span>
-            <span>2</span>
-            <span>3</span>
-            <span>4</span>
+          <div class="flex w-3/4 md:w-1/2 my-4">
+            <a href="#" class="w-1/4 flex h-8">
+              <img :src="github" alt="GitHub" />
+            </a>
+            <a href="#" class="w-1/4 flex h-8">
+              <img :src="twitter" alt="Twitter" />
+            </a>
+            <a href="#" class="w-1/4 flex h-8">
+              <img :src="linkedin" alt="LinkedIn" />
+            </a>
+            <a href="#" class="w-1/4 flex h-8">
+              <img :src="mail" alt="Mail" />
+            </a>
           </div>
         </div>
       </div>
-      <div class="bg-yellow-300 w-full md:w-1/3 flex items-center justify-center">
-        <img :src="profile" alt="Profile" class="w-3/4 md:w-full" />
+      <div class="w-full md:w-1/3 flex items-center justify-center md:-ml-24 md:-mt-24">
+        <img :src="profile" alt="Profile" class="w-3/4 floating" />
       </div>
     </section>
   </main>
@@ -35,6 +45,10 @@
 <script>
 import profile from '@/assets/profile-white.png';
 import Navbar from '@/components/Navbar.vue';
+import github from '@/assets/icons/github.svg';
+import twitter from '@/assets/icons/twitter.svg';
+import linkedin from '@/assets/icons/linkedin.svg';
+import mail from '@/assets/icons/mail.svg';
 
 export default {
   components: {
@@ -42,7 +56,11 @@ export default {
   },
   data(){
     return{
-      profile
+      profile,
+      github,
+      twitter,
+      linkedin,
+      mail
     }
   }
 }
